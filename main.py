dfdc63dc2c56fdf5144354d47e0ffed85ca63c06
akaydo <tecno4kaa@mail.ru>
2022-10-15 14:57:49 +0300

    create function encrypt

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ def get_question(number_q):
             question = q_answer[0:i]
             answer = q_answer[i + 1:len(q_answer)].lower()
     return answer, question
+
+def encrypt(answer):
+    curent_view = []
+    for i in range(0, len(answer)):
+        curent_view.append('*')
+    return curent_view
