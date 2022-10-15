7b3d3c84baa2066cb9e33f65c4fa6988a5345544
akaydo <tecno4kaa@mail.ru>
2022-10-15 14:03:41 +0300

    create function get_question and add questions

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from main import *
 def test_1():
     try:
         a,q = get_question(1)
-        assert a == "приз" and q ==  "сектор в игре <Поле чудес>"
+        assert a == "приз" and q == "сектор в игре <Поле чудес>"
     except:
         print('test error')
     else:
